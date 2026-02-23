f0bd814e249943121a9ec89435513ef986c637e9
arya1228 <priyankasrivastava89@gmail.com>
2026-02-23 14:15:21 +0530

    Formatted code

--- Assignment-1/ToDoApp/parent/common.tf
+++ Assignment-1/ToDoApp/parent/common.tf
@@ -2,16 +2,14 @@ module "res" {
   source  = "../child/resource_group"
   rg_name = "Akhnoor"
   rg_loc  = "Central India"
-
   rg_name1 = "Angoor"
-  rg_loc1 = "East US"
+  rg_loc1  = "East US"
 }
 
 
 module "storage" {
-  source = "../child/Storage"
+  source  = "../child/Storage"
   rg_name = module.res.rg_name
-  rg_loc = module.res.rg_loc
-  
+  rg_loc  = module.res.rg_loc
 }
 
