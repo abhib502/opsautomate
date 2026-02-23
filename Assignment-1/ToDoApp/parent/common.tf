fbcfed2e3decb6ca12ed31757f6d7d1b2f5c3311
arya1228 <priyankasrivastava89@gmail.com>
2026-02-23 13:44:18 +0530

    Assignment-1 Code added

---
+++ Assignment-1/ToDoApp/parent/common.tf
@@ -0,0 +1,5 @@
+module "res" {
+  source  = "../child/resource_group"
+  rg_name = "Akhnoor"
+  rg_loc  = "Central India"
+}
---
+++ Assignment-1/ToDoApp/parent/provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.61.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+  subscription_id = "f3d8bbb6-08bd-4642-b600-a9ed0a03587f"
+}
