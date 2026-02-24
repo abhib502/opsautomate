6660646e386db0834b33bbdf693a441bc08d2ae4
arya1228 <priyankasrivastava89@gmail.com>
2026-02-24 13:06:35 +0530

    New RG added

--- Assignment-1/ToDoApp/child/resource_group/variables.tf
+++ Assignment-1/ToDoApp/child/resource_group/variables.tf
@@ -3,3 +3,6 @@ variable "rg_loc" {}
 
 variable "rg_name1" {}
 variable "rg_loc1" {}
+
+variable "rg_name2" {}
+variable "rg_loc2" {}
--- Assignment-1/ToDoApp/parent/common.tf
+++ Assignment-1/ToDoApp/parent/common.tf
@@ -1,9 +1,11 @@
 module "res" {
-  source  = "../child/resource_group"
-  rg_name = "Akhnoor"
-  rg_loc  = "Central India"
+  source   = "../child/resource_group"
+  rg_name  = "Akhnoor"
+  rg_loc   = "Central India"
   rg_name1 = "Angoor"
   rg_loc1  = "East US"
+  rg_name2 = "Akhil"
+  rg_loc2  = "West US"
 }
 
 
