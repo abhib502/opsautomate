870c91227068ed35dcf4a7185d7b4eea2d6c2aa3
arya1228 <priyankasrivastava89@gmail.com>
2026-03-16 13:25:54 +0530

    Problem Solved

--- Assignment-1/ToDoAppPrac/parent/common.tf
+++
@@ -1,5 +0,0 @@
-module "rg" {
-  source  = "../child/resource_group"
-  rg_name = "akhnoor"
-  rg_loc  = "Central India"
-}
---
+++ Assignment-1/ToDoAppPrac/parent/variables.tf
@@ -0,0 +1,7 @@
+variable "rg_name" {}
+variable "rg_loc" {}
+variable "rg2" {}
+variable "rg2_loc" {}
+variable "rg3" {}
+variable "rg3_loc" {}
+variable "stg_name" {}
